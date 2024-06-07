ef17047144e2c4df3c00cfbc3e8e97ecea438837
Michal Holub <holub@shoptet.cz>
2024-06-07 09:53:46 +0200

    Initial commit

---
+++ calculate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+print_primes() {
+    N=$(cat max_number)
+    for ((i=2; i<=N; i++)); do
+        sleep 1
+        is_prime=1
+        for ((j=2; j*j<=i; j++)); do
+            if ((i % j == 0)); then
+                is_prime=0
+                break
+            fi
+        done
+        if ((is_prime)); then
+            echo $i
+        fi
+    done
+}
+
+print_primes
