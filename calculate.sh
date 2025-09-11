a4f556ea93ed4d804c78f391885d5b53e396cddd
Ondrej Moravek <moravek@shoptet.cz>
2025-09-11 12:23:29 +0200

    new `pull_request` only workflow with path filter

--- calculate.sh
+++ calculate.sh
@@ -4,7 +4,7 @@ print_primes() {
     N=$(cat max_number)
     for ((i=2; i<=N; i++)); do
         is_prime=1
-	sleep 1
+        sleep 1
         for ((j=2; j*j<=i; j++)); do
             if ((i % j == 0)); then
                 is_prime=0
